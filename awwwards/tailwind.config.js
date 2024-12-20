093bace2d7bffcc1ef59f871b8feb53cd1478bac
RKP1202 <b22es003@iitj.ac.in>
2024-12-20 22:54:56 +0530

    Initial commit

--- awwwards/tailwind.config.js
+++ awwwards/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./src/**/*.{html,js}"],
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      zentry : ['zentry' , 'sans-serif'],
+      
+    },
   },
   plugins: [],
 }
